926bff2f72c01841db87bd88da41e1548b6ded68
Oleg <olegberezin2002@gmail.com>
2022-09-15 18:33:03 +0300

    input function written

--- The Tragedy of Hamlet, Prince of Denmark/io.cpp
+++ The Tragedy of Hamlet, Prince of Denmark/io.cpp
@@ -2,9 +2,36 @@
 
 void input(FILE *fp) {
     fp = fopen("text.txt", "r");
-
     if (fp == NULL) {
         perror("fopen() failed");
         return;
     }
+
+    fseek(fp, 0, SEEK_END);
+    size_t fsize = (size_t) ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+
+    // printf("%d\n", fsize);  
+
+    char *buffer = (char*) calloc(fsize + 1, sizeof(char));
+    if (buffer == NULL) {
+        perror("calloc() failed");
+        return;
+    }
+
+    // printf("%d\n", fsize);  
+
+
+    size_t result = fread(buffer, 1, fsize, fp);
+    if (result != fsize) {
+        perror("fred() failed");
+        return;
+    }
+
+    fclose(fp);
+    
+    // puts(buffer);
+    free(buffer);
+
+    return;
 }
--- The Tragedy of Hamlet, Prince of Denmark/io.hpp
+++ The Tragedy of Hamlet, Prince of Denmark/io.hpp
@@ -2,6 +2,7 @@
 #define _IO_H_
 
 #include <stdio.h>
+#include <stdlib.h>
 
 void input(FILE *fp);
 void solve(FILE *fp);
